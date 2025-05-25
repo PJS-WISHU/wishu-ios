ad1bd688a0ffb2fddba8d8af31a62d8c14d12979
kermit715 <keye.e.dev@gmail.com>
2025-05-25 13:20:07 +0900

    Initial Commit

---
+++ wishu/wishuApp.swift
@@ -0,0 +1,17 @@
+//
+//  wishuApp.swift
+//  wishu
+//
+//  Created by 김은영 on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct wishuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wishuTests/wishuTests.swift
@@ -0,0 +1,16 @@
+//
+//  wishuTests.swift
+//  wishuTests
+//
+//  Created by 김은영 on 5/25/25.
+//
+
+import Testing
+
+struct wishuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
